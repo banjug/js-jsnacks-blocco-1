4ff7c76fd6ec7f1f26f16ceb4cb7fc2eceb6ffff
Samuele Miotto <miotto.samuele@gmail.com>
2021-10-28 15:53:57 +0200

    jsnack 1 complete

---
+++ jsnack1/script.js
@@ -0,0 +1,8 @@
+const firstNum = parseInt(prompt("Inserisci il primo numero."));
+const secondNum = parseInt(prompt("Inserisci il secondo numero."));
+
+console.log("Il primo numero è: " + firstNum);
+console.log("Il secondo numero è: " + secondNum);
+
+const numberSum = firstNum + secondNum;
+console.log("La somma dei numeri è: " + numberSum);
