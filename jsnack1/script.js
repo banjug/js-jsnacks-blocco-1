7fb7138dc8df6d370ecdef2a405aec505e2f60fe
Samuele Miotto <miotto.samuele@gmail.com>
2021-10-29 09:41:28 +0200

    fix jsnack 1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,25 +1,18 @@
-// const firstNum = parseInt(prompt("Inserisci il primo numero."));
-// const secondNum = parseInt(prompt("Inserisci il secondo numero."));
-
-// console.log("Il primo numero è: " + firstNum);
-// console.log("Il secondo numero è: " + secondNum);
-
-// const numberSum = firstNum + secondNum;
-// console.log("La somma dei numeri è: " + numberSum);
-
-
-
 const sumButton = document.getElementById("sumBtn");
-const sumCont = document.getElementById("container");
+const numCont = document.getElementById("container");
 
 sumButton.addEventListener('click', 
 function() {
 
         let firstNum = parseInt(document.getElementById("numb1").value);
         let secondNum = parseInt(document.getElementById("numb2").value);
-        let numberSum = firstNum + secondNum;        
-        sumCont.innerHTML = `La somma dei due numeri è ${numberSum}`;
-        console.log(numberSum);
+        if (firstNum > secondNum) {
+            numCont.innerHTML = `Il numero maggiore è ${firstNum}`;
+        } else if (firstNum < secondNum) {
+            numCont.innerHTML = `Il numero maggiore è ${secondNum}`;
+        } else {
+            numCont.innerHTML = `I due numeri sono uguali`;
+        }
     }
     
 );
